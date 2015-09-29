524f060080738d312230570bbf381e39e9935e60
Carol Willing <carolcode@willingconsulting.com>
2015-09-29 05:45:49 -0700

    Add maxdiff for test

--- mysite/customs/tests.py
+++ mysite/customs/tests.py
@@ -921,6 +921,7 @@ class ExportTrackerAsDict(django.test.TestCase):
                   'existing_bug_urls': [],
                   'get_older_bug_data': None,
                   }
+        self.maxdiff = None
         self.assertEqual(golden, exported)
 
     def test_export_includes_existing_bugs(self):
